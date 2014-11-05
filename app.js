f4f616bf17b6772661720822174bcb8c6955086a
princessjerome <darkhero185@hotmail.com>
2014-11-04 21:50:18 -0500

    Previous files

---
+++ app.js
@@ -0,0 +1,30 @@
+var pre=function(){
+	
+	//$('#head1').hide();
+	$('.pic').hide();
+	$('#head1').animate({ opacity: 0, 'marginTop':"-105px" }, 0);
+	$('#content').hide();
+
+};
+
+var main=function(){
+
+	$(document).ready(function(){
+		$('.pic').fadeIn(4000);	
+		$('#head1').animate({opacity:1, 'marginTop':"-10px"}, 1800);
+		$('#content').fadeIn(4000);	
+	});
+	
+	$('#scbut').click(function(e){
+	
+	  e.preventDefault();
+	
+	$('html,body').animate({
+	scrollTop: $("#schd1").offset().top-25	
+	}, 250);
+	});
+
+};
+
+$(pre);
+$(document).ready(main);
